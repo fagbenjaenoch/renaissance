93c1916b1c7c003add1e52f242b5355d33b46b3b
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-07-14 10:32:17 +0100

    added accessilibity check in ticker component

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,28 @@ function App() {
 				<Ticker />
 			</section>
 			<section className="about">
-				<h2>Why does this page exist?</h2>
+				<div className="wrapper">
+					<h2>Why does this page exist?</h2>
+					<p>
+						Nothing really serious. It's been almost 7 months I've written code
+						and this was because I didn't have a personal computer after my
+						previous semi-laptop(not kidding) was damaged.
+					</p>
+					<p>
+						It was really tough for me. I lost my Job and lost a bit of
+						confidence but I held on to hope--the hope that one day I will get a
+						new laptop. I had to be delusional a bit and I was always searching
+						for a new laptop to buy but I didn't have the money.
+					</p>
+					<p>
+						Not until I broke out of my silence and reached out to an uncle of
+						mine who decided to sponsor me and provide me with a new laptop.
+					</p>
+					<p>
+						An that was how I came up with the word "Renaissance" just like how
+						it was a revival of intellectualism after antiquity.
+					</p>
+				</div>
 			</section>
 		</div>
 	);
--- src/components/ticker.tsx
+++ src/components/ticker.tsx
@@ -1,20 +1,23 @@
-// import { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import "./ticker.css";
 
 export default function Ticker() {
-	// const containerRef = useRef(null)
-	// useEffect(() => {
-	// 	if(!window.matchMedia("(prefer-reduced-motion: reduce)").matches) {
-	// 		addAnimation()
-	// 	}
+	const containerRef = useRef<HTMLDivElement>(null);
+	useEffect(() => {
+		if (!window.matchMedia("(prefer-reduced-motion: reduce)").matches) {
+			addAnimation();
+		}
 
-	// 	function addAnimation() {
-	// 		containerRef.current.setAttribute("data-animated", true)
-	// 	}
-	// }, [])
+		function addAnimation() {
+			if (!containerRef.current) {
+				return;
+			}
+			containerRef.current.setAttribute("data-animation", "true");
+		}
+	}, []);
 
 	return (
-		<div className="ticker-container">
+		<div className="ticker-container" ref={containerRef}>
 			<div className="ticker-inner">
 				<div className="ticker-item">Renaissance</div>
 				<div className="ticker-item">Renaissance</div>
