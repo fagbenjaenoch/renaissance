7ccee1f375661c017e4de3b5171af3bc3496a1ab
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-09-27 16:31:11 +0100

    added tailwind to the project

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}", // Adjust path as needed for your project structure
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
